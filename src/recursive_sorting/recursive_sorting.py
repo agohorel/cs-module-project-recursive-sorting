fe536aab21373233d2a4204053a92506219bfcc9
agohorel <3317203+agohorel@users.noreply.github.com>
2020-05-19 22:24:19 -0400

    in-place mergesort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -36,8 +36,8 @@ def merge_sort(arr):
         return arr
     else:
         mid = (len(arr)) // 2
-        left_array = arr[0:mid]
-        right_array = arr[mid:len(arr)]
+        left_array = arr[:mid]
+        right_array = arr[mid:]
 
         left_array = merge_sort(left_array)
         right_array = merge_sort(right_array)
@@ -47,16 +47,46 @@ def merge_sort(arr):
 
 
 # implement an in-place merge sort algorithm
-
-
 def merge_in_place(arr, start, mid, end):
-    # Your code here
+    start_hi = mid + 1
 
-    return arr
+    # if mid and start_hi already in order, return
+    if arr[mid] <= arr[start_hi]:
+        return
+    # while pointers are within bounds
+    while start <= mid and start_hi <= end:
+        # if start elem is in order w/ respect to start_hi elem
+        if arr[start] <= arr[start_hi]:
+            # just increment start
+            start += 1
+        else:
+            # current value is at start of start_hi
+            value = arr[start_hi]
+            # index is start_hi
+            index = start_hi
+
+            # while index is not equal to start
+            while index != start:
+                # swap item at index with it's left-neighbor
+                arr[index] = arr[index-1]
+                # decrement index (working toward exiting above while loop)
+                index -= 1
+            # value at arr start is new value (the other half of the swap)
+            arr[start] = value
+            # increment all pointers (except end)
+            start += 1
+            mid += 1
+            start_hi += 1
 
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
+    if l < r:
+        mid = (l + r) // 2
+
+        merge_sort_in_place(arr, l, mid)
+        merge_sort_in_place(arr, mid+1, r)
+
+        merge_in_place(arr, l, mid, r)
 
     return arr
 
@@ -67,3 +97,8 @@ def timsort(arr):
     # Your code here
 
     return arr
+
+
+arr = [8, 7, 6, 5, 4, 3, 2, 1]
+result = merge_sort_in_place(arr, 0, len(arr)-1)
+print(result)
